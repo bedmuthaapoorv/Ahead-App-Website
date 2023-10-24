28a6d1149594a6640b415f6af0eb0638306bfbfb
bedmuthaapoorv <apoorvbedmutha@gmail.com>
2023-10-24 23:02:09 +0530

    added the Description component

--- dynamic-website/src/App.js
+++ dynamic-website/src/App.js
@@ -2,12 +2,16 @@ import './stylessheets/App.css'
 import NavBar from './NavBar';
 import Board from './Board';
 import LandingPage from './LandingPage';
+import Description from './Description';
+import EmotionCarousal from './EmotionCarousal';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Board comp={<LandingPage />}></Board>
+      <Description></Description>
+      <EmotionCarousal></EmotionCarousal>
     </div>
   );
 }
---
+++ dynamic-website/src/Description.js
@@ -0,0 +1,16 @@
+import './stylessheets/Description.css'
+export default function Description(props) {
+    return (
+        <div className='Description_Wrapper'>
+            <div className='Description_Rows'>
+                <div className='mediumText Description_RowElement'>EQ beats IQ</div>
+                <div className='Description_RowElement'>People with high emotional intelligence (EQ)
+                    live more fulfilled lives. They tend to be happier and have
+                    healthier relationships</div>
+                <div className='Description_RowElement'>They are more successful in their pursuits and make for inspiring
+                    leaders.According to science, they earn $29k a year.
+                </div>
+            </div>
+        </div>
+    );
+}
